e0136d491b3aa6a11fb686cc8894bf63e634978a
Cenachi <carloscenachi@protonmail.com>
2022-05-09 03:37:18 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Count from "./components/Count.vue";
+</script>
+
+<template>
+  <main id="app">
+    <Count />
+  </main>
+</template>
+
+<style>
+* {
+  background-color: rgb(43, 43, 43);
+}
+</style>
---
+++ src/components/Count.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="container">
+    <button class="button" @click="increment">Count is: {{ count }}</button>
+
+    <button class="button" @click="reset">Reset</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+    };
+  },
+
+  methods: {
+    increment() {
+      this.count++;
+    },
+
+    reset() {
+      this.count = 0;
+    },
+  },
+};
+</script>
+
+<style>
+#container {
+  display: flex;
+  justify-content: center;
+}
+
+.button {
+  background-color: rgb(0, 255, 213);
+  font-size: 25px;
+  font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS", sans-serif;
+}
+</style>
